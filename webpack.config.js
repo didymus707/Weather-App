239da3f40051d21c43c126bc624558b0e1cd7987
Adewale Orotayo <didymus7007@gmail.com>
2020-11-24 16:39:05 +0100

    Added output key and value in module.exports in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  path: path.resolve(__dirname, 'dist'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
