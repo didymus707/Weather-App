6226b0f9a06f6de4dedef5e7970090518c2f6923
Adewale Orotayo <didymus7007@gmail.com>
2020-11-25 14:09:59 +0100

    Created getData to fetch data from api Installed htmlwebpackplugin and configured it in webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './assets/styles/main.css';
+
+const getData = async () => {
+  const cityName = 'Nigeria';
+  const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={API key}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
