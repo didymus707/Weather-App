1b352648497656ccc2eae7ea0340a69b4e6f8bf7
Adewale Orotayo <didymus7007@gmail.com>
2020-11-25 14:22:38 +0100

    Added devtool in webpack.config for development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Weather App',
