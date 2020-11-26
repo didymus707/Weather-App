cfe322d0f8127b911a2d42cf4000ac5302922049
Adewale Orotayo <didymus7007@gmail.com>
2020-11-26 17:53:24 +0100

    Installed regenerator-runtime Created processData to process result from api call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './assets/styles/main.css';
+import "regenerator-runtime/runtime.js"; //eslint-disable-line
 
 const getData = async () => {
   try {
@@ -6,10 +7,19 @@ const getData = async () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1167e47fcac43a43c26e928ef53a787b`;
     const response = await fetch(url, { mode: 'cors' });
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    return data;
   } catch (error) {
-    console.log(`Error:  ${error}`);
+    throw new Error(`Error:  ${error}`);
   }
 };
 
-getData();
+const processData = async () => {
+  const result = await getData();
+  const {
+    name, clouds, main, sys, weather,
+  } = result;
+  console.log(name, clouds, main, sys, weather);
+};
+
+processData();
